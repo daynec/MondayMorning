2ee03319c42a0a081bd79a1cfc99d15f8a32f5c0
Bobby Tables <brandon.kristian.bell@gmail.com>
2019-03-21 06:40:30 -0500

    Compiles on my VM

--- MondayMorning/src/application/Main.java
+++ MondayMorning/src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource(“/main.fxml”));
+			Parent root = FXMLLoader.load(getClass().getResource("../application/view/Main.fxml"));
 			primaryStage.setScene(new Scene(root, 500, 575));
 			primaryStage.show();
 		} catch(Exception e) {
